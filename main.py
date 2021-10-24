3d8bcc5f004f7b9e3b1c537b16862fbe303f8d81
Padsala Trushal <57517785+Trushal2004@users.noreply.github.com>
2021-10-24 17:11:01 +0530

    add function for extraction files from debian package

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+
+'''
+Tool Name ::
+Author :: Padsala Trushal #changing this line doesn't make you developer
+Date :: 24 Oct 2021
+'''
+
+import os
+
+
+filename = str(input("enter your file name:\n"))
+def extractor(filename):
+	command = f"dpkg-deb -R {filename} ./test"
+	os.system(command) 
+
+extractor(filename)
 
