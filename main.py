dafbcb4699fc5a0dfffd2628d025dedfd1d1acdb
Padsala Trushal <57517785+Trushal2004@users.noreply.github.com>
2021-10-27 10:32:05 +0530

    Add function for command line usages

--- main.py
+++ main.py
@@ -7,12 +7,51 @@ Date :: 24 Oct 2021
 '''
 
 import os
+import argparse
+import sys
+
+
+def get_arguments():
+	parser = argparse.ArgumentParser(description='Inject bash script in debian package',usage=f'python3 {sys.argv[0]} -p debian_package -s bash_script',epilog=f'EXAMPLE - python3 {sys.argv[0]} -p /tmp/file.deb -s /tmp/script.sh')
+
+	parser.add_argument('-v','--version',action='version',version='1.4',help='show the version of program')
+
+	parser.add_argument('-s',metavar='bash script',dest='bash',help='Enter your bash script path')
+
+
+	parser.add_argument('-p',metavar='debian package',dest='deb',help='Enter your debian package path')
+
+	args = parser.parse_args()
+
+	if len(sys.argv) < 2:
+		parser.print_usage()
+		sys.exit(1)
+		
+	return args
+
+
+
+
+
+
+
+
+
+
+
 
 
 filename = str(input("enter your file name:\n"))
+b = filename.split(".deb")
+
 def extractor(filename):
-	command = f"dpkg-deb -R {filename} ./test"
+	command = f"dpkg-deb -R {filename} ./{b[0]}"
 	os.system(command) 
+	print("[*] Package Extract Successfully")
 
 extractor(filename)
+
+
+
+
 
